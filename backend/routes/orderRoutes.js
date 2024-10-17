e1f46fa4baf7e387c6f5d98fe1d26959dda8b58a
amrendrayd <amrendrayd3@gmail.com>
2024-10-17 10:08:59 +0530

    update

--- backend/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { placeOrder } = require('../controllers/orderController');
-const protect = require('../middlewares/authMiddleware');
-const router = express.Router();
-
-router.post('/', protect, placeOrder);
-
-module.exports = router;
--- backend/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getAllProducts, addProduct } = require('../controllers/productController');
-const protect = require('../middlewares/authMiddleware');
-const router = express.Router();
-
-router.get('/', getAllProducts);
-router.post('/', protect, addProduct);
-
-module.exports = router;
